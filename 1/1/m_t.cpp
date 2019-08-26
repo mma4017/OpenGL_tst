3a4630d06da388a65789240c85c7d5d0b501648f
Haiyao <mma4017@gmail.com>
2019-08-26 16:58:44 +0800

    PS

--- 1/1/m_t.cpp
+++ 1/1/m_t.cpp
@@ -3,6 +3,9 @@
 #include <GL/glew.h>
 // GLFW
 #include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
@@ -11,6 +14,16 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
 
     return 0;
 }
